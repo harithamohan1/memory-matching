aa37ec30cdbce8e4c4cc77d44cce2b7c34a9ea74
harithamohan1 <harithamohan10@gmail.com>
2021-05-07 18:25:10 -0400

    updated file

---
+++ key.js
@@ -0,0 +1,7 @@
+export const key = {
+    NASA_API_URL: 'https://api.nasa.gov/planetary/apod?api_key=GwspDD3cQrVRD5WJ7TvkpZEHsmv5bwGrX7ItWD6G',
+}
+
+export const client_key = {
+    client: 'Ai5246OBbT3WKJeVZ2lpUyZW0Rcn34qrp_hVXCz_IOY',
+}
